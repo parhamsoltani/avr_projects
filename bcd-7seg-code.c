ded5e999680dbe40d4db50346765affe14464aae
parhamsoltani <70743729+parhamsoltani@users.noreply.github.com>
2023-07-12 11:24:13 +0330

    Update bcd-7seg-code.c

--- bcd-7seg-code.c
+++ bcd-7seg-code.c
@@ -1,12 +1,13 @@
 #include<mega32.h>
- 
+
+#in C you don't have to mention array's length when declaring it
 Flash unsigned char bcd_7seg[11]={0x3f,0x06,0x58,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x80};
  
 Voidmain()
  
 {
  
-	Unsignedcharbcd;
+	Unsigned char bcd;
  
 	DDRB=0XFF;
  
