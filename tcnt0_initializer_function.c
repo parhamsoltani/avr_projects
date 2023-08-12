7b9d31f6f18e13e40107e5f8034147a1404598d8
parhamsoltani <70743729+parhamsoltani@users.noreply.github.com>
2023-08-12 17:10:26 +0330

    Update tcnt0_initializer_function.c

--- tcnt0_initializer_function.c
+++ tcnt0_initializer_function.c
@@ -73,7 +73,38 @@ void timer0_init(long int mcu_clock,int prescaler, int req_delay)
             TIMSK |= (1<<TOIE0);
             counter_overflow = 0;
             break;
+    }
+
+/*
+    TCCR0 |= (1<<CS02);
+    TCNT0=0x00;
+    TIMSK |= (1<<TOIE0);
+    counter_overflow = 0; 
+*/
+
+    timer_clock = mcu_clock/prescaler;
+    counting_time = 1000.0/timer_clock;
+    
+    overflow_time = counting_time*256;
+    x = req_delay/counting_time;
+
+    if (req_delay >= overflow_time)
+    {
+        tot_overflow = (req_delay/overflow_time);
+        remaining_time = req_delay - overflow_time*tot_overflow;
+        remaining_counts = remaining_time/counting_time;
+        remainder_time = 255 - remaining_counts;
+    }
+    else
+    {
+        tot_overflow=0;
+        remainder_time = 255 - x; 
+        TCNT0=remainder_time;
+    }
+
+
 }
+
 
 int main() {
     int prescaler = 1024;
